c24268bee37ee40bd24466c0f27f4cc14037cdf3
Alexey Vetkhov <vethov.alexey@gmail.com>
2016-05-06 15:19:05 +0300

    Add gunicorn to project

--- hello.py
+++ hello.py
@@ -1,16 +1,9 @@
 import urlparse
 
-def wsgi_app(environ, start_response):
+def app(environ, start_response):
 	params = urlparse.parse_qs(environ["QUERY_STRING"])
 
-	body = "query strings:\n"
-	for k in params.iterkeys():
-		body += k + ":\n"
-		body += "\n ".join(params[k]) + "\n\n"
+	start_response("200 OK", [("Content-Type", "text/plain")])
 
-	start_response("200 OK",
-			[("Content-Type", "text/plain"),
-			("Content-Length", str(len(body)))]
-
-	return [body]
+	return ['%s=%s<br>' % (k, params[k][0]) for k in params]
 	
